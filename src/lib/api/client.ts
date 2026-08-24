195d68bc67a60a814aae80ff7affd6ea2feb6c8e
Hifza-155 <hafsausman1512@gmail.com>
2026-08-24 19:05:21 +0500

    supabase URL and anon key moved to . env file and is accessed in client.ts through it

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
+import { env } from '../config/env';
 
-const supabaseUrl = 'https://fqpizscquprqqubsaymq.supabase.co';
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxcGl6c2NxdXBycXF1YnNheW1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU5NDY5NjUsImV4cCI6MjEwMTUyMjk2NX0.2VbuUfMTDsG-eqEisfCYDTeqKf1852uzkYKf6RwonmA";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  env.NEXT_PUBLIC_SUPABASE_URL,
+  env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
