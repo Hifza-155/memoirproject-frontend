4a571fce0ffc94a7cec57ce789f726890787d585
Hifza-155 <hafsausman1512@gmail.com>
2026-08-29 22:07:09 +0500

    Unecessary import removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@
 
 import Hero from "../features/landingPage/Hero";
 import LandingPageScroll from "../features/landingPage/LandingPageScroll";
-import OwnerDashboard from "../features/dashboard/OwnerDashboard";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <LandingPageScroll />
-      <OwnerDashboard />
     </main>
   );
 }
